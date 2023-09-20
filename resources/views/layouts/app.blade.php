f606fe441024dd49d92b10a076f021e7ddb0c5b1
wuhibe <wuhibetamire710@gmail.com>
2023-09-20 18:57:33 +0300

    fix: sidebar hang issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
         <!-- Bootstrap CSS -->
         <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
         <!-- Add your custom CSS styles here -->
+        <link href="{{asset('css/all.min.css')}}" rel="stylesheet" />
         <link href="{{ asset('css/custom.css') }}" rel="stylesheet" />
         <script src="{{ asset('js/sweet_alert.js') }}"></script>
         <!-- CSS Front Template -->
@@ -16,22 +17,19 @@
             href="{{ asset('css/theme.minc619.css') }}?v=2.0"
         />
     </head>
-    <body style="height:100vh; width:100vw">
+    <body style="height: 100vh; width: 100vw; display: flex; flex-direction: column;">
         @include('layouts.header')
-
-        <div style="height:90%">
-            <div class="row p-0 m-0" style="height:100%" >
-                @include('layouts.sidebar')
-
-                <main role="main" class="col-md-9 ml-sm-auto col-lg-10">
-                    <div class="py-2"></div>
+        <div style="flex-grow: 1; display: flex;">
+            @include('layouts.sidebar')
+            <main role="main" style="flex-grow: 1; overflow-y: auto;">
+                <div class="py-2"></div>
+                <div style="position: sticky; top: 0; background-color: #fff; z-index: 1;">
+                    <!-- Your main content here -->
                     @yield('content')
-                </main>
-            </div>
+                </div>
+            </main>
         </div>
-
         <!-- Include Bootstrap JS and your custom JS scripts here -->
-        <link href="{{asset('css/all.min.css')}}" rel="stylesheet"/>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
