9cc9f97974dddb01e6044bfbc7d9b1c1db392550
wuhibe <wuhibetamire710@gmail.com>
2023-10-01 11:40:31 +0300

    fix: hide revenue numbers for receptionist

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,6 +4,7 @@
     <div class="container">
         <h1>Dashboard</h1>
 
+        @if(auth('admin')->user()->user_type == 'admin')
         <div class="row">
             <div class="col-md-4">
                 <div class="card">
@@ -35,6 +36,7 @@
         <div class="d-flex justify-content-end">
             <a href="{{ route('dashboard.details') }}" class="ml-2 mb-2 mt-2">Show More Details</a>
         </div>
+        @endif
         <div class="mt-4">
             <h2>Upcoming Reservations</h2>
             <table class="table">
